37c4bc0cc419881dcf51178fb30a6009caed894c
bsao17 <declic62@gmail.com>
2020-12-11 08:35:12 +0100

    Add new entity image and fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,7 +3,6 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
-use Cocur\Slugify\Slugify;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -11,13 +10,11 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $slugify = new Slugify();
+        
         
         for ($i=0; $i<=30; $i++){
             $ad = new Ad;
-            $title = $slugify->slugify("titre-de-l-annonce-n-$i");
             $ad->setTitle("titre de l'annonce n°$i")
-                ->setSlug($title)
                 ->setCoverImage("https://via.placeholder.com/1000x300")
                 ->setIntroduction("voici ma nouvelle annonce qui devrait intéresser beaucoup de monde")
                 ->setContent("<p>Je suis le contenu de la nouvelle annonce de Bruno</p>")
--- src/Entity/Ad.php
+++ src/Entity/Ad.php
@@ -3,10 +3,14 @@
 namespace App\Entity;
 
 use App\Repository\AdRepository;
+use Cocur\Slugify\Slugify;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=AdRepository::class)
+ * @orm\HasLifecycleCallbacks
  */
 class Ad
 {
@@ -51,6 +55,31 @@ class Ad
      * @ORM\Column(type="integer")
      */
     private $rooms;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Image::class, mappedBy="ad")
+     */
+    private $images;
+
+    public function __construct()
+    {
+        $this->images = new ArrayCollection();
+    }
+
+    /**
+     * Permet d'initialiser un slug vide
+     * @orm\PrePersist
+     * @orm\PreUpdate
+     *
+     * @return void
+     */
+    public function initSlug()
+    {
+        if(empty($this->slug)){
+            $slugify = new Slugify();
+            $this->slug = $slugify->slugify($this->title);
+        }
+    }
 
     public function getId(): ?int
     {
@@ -137,6 +166,36 @@ class Ad
     public function setRooms(int $rooms): self
     {
         $this->rooms = $rooms;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Image[]
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    public function addImage(Image $image): self
+    {
+        if (!$this->images->contains($image)) {
+            $this->images[] = $image;
+            $image->setAd($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Image $image): self
+    {
+        if ($this->images->removeElement($image)) {
+            // set the owning side to null (unless already changed)
+            if ($image->getAd() === $this) {
+                $image->setAd(null);
+            }
+        }
 
         return $this;
     }
